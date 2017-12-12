76b49187f6a15be0d2de485f23d65e46f7f5ce75
Niall Nulty <S00162826@mail.itsligo.ie>
2017-12-12 23:36:11 +0000

    part 2 completed

---
+++ Products/Controllers/Category.cs
@@ -0,0 +1,28 @@
+﻿using Products.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Products.Controllers
+{
+    [Table("Category")]
+
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Category ID")]
+
+        public int CategoryId { get; set; }
+
+        [Display(Name = "Category Name")]
+
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> productsInCategory { get; set; }
+
+    }
+}
---
+++ Products/Models/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Products.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Products.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
